1bdeaa6d2595d17c3c58454024116da356604cae
Robert Rudolph <robert.rudolph@mailbox.org>
2026-03-01 23:39:25 +0100

    Update cask SHA256 for v1.8

--- Casks/netfluss.rb
+++ Casks/netfluss.rb
@@ -1,6 +1,6 @@
 cask "netfluss" do
   version "1.8"
-  sha256 "PLACEHOLDER"
+  sha256 "6231da75541300201e3ebad1e0af164f9e8eb033908414ad44398da4324181b5"
 
   url "https://github.com/rana-gmbh/netfluss/releases/download/v#{version}/Netfluss-#{version}.zip"
   name "Netfluss"
