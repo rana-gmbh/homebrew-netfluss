afeae20707ea4236968220c21a2d51dac0ec9fd7
rana-gmbh <rana-gmbh@users.noreply.github.com>
2026-04-14 09:30:00 +0200

    Release NetFluss 2.1.1

--- Casks/netfluss.rb
+++ Casks/netfluss.rb
@@ -1,6 +1,6 @@
 cask "netfluss" do
-  version "2.1"
-  sha256 "56d2386cb0b5c63902fe28e333df4c89bd17db09519d01ccbe70c65dcc90afea"
+  version "2.1.1"
+  sha256 "0b24d54a8cd0a8d0d05e7ce39ce46148b8a8f33bc41c7b0156fb79a08d91f665"
 
   url "https://github.com/rana-gmbh/NetFluss/releases/download/v#{version}/NetFluss-#{version}.zip"
   name "NetFluss"
