1f7bc522f019c21ddf6cc64b88711c2365f5fe9c
Robert Rudolph <robertrudolph@Roberts-MacBook-Pro.local>
2026-04-26 08:24:50 +0200

    Update NetFluss to 2.2.1

--- Casks/netfluss.rb
+++ Casks/netfluss.rb
@@ -1,6 +1,6 @@
 cask "netfluss" do
-  version "2.2"
-  sha256 "8879ab6e4628459751b67bc801f1676fe393e4bedb941a7d29b5570e7bfcb6f9"
+  version "2.2.1"
+  sha256 "58f2d6c75fed58340e81ee761123218309f94e88efdb62a5ac41dc06abfeb718"
 
   url "https://github.com/rana-gmbh/NetFluss/releases/download/v#{version}/NetFluss-#{version}.zip"
   name "NetFluss"
